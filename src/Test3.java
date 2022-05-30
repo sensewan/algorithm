2dc1e3c542bda04651fc4ce169cd2a4119285684
sensewan <sensewan@gmail.com>
2022-05-30 14:20:17 +0900

    test 파일 추가

--- src/Test3.java
+++ src/Test3.java
@@ -1,14 +1,15 @@
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Test3 {
     public static void main(String[] args) throws IOException {
 
-        System.out.println("디버깅 포인트1");
 
-        System.out.println("디버깅 포인트2");
 
 
         double d = 1.000056;
@@ -29,6 +30,142 @@ public class Test3 {
         for(String s : list){
             System.out.print(s);
         }
+
+        System.out.println();
+
+        double a = 10000.0;
+        double b = 100.0;
+
+        System.out.println(a+b);
+
+        System.out.println();
+
+        double dd1 = 0.00001;
+        double dd2 = 0.00002;
+        System.out.println(dd1+dd2);
+
+        BigDecimal bc1 = BigDecimal.valueOf(dd1);
+        BigDecimal bc2 = BigDecimal.valueOf(dd2);
+        System.out.println(bc1);
+        System.out.println(bc2);
+
+        BigDecimal sum = null;
+        sum = bc1.add(bc2);
+        sum = sum.add(bc2).stripTrailingZeros();
+        System.out.println("sum: " +sum);
+        System.out.println("sum 곱하기: " +sum.multiply(BigDecimal.valueOf(3)).stripTrailingZeros());
+
+
+        System.out.println("-----------------------------------------------------");
+
+
+        double d1 = 1000.0;
+        double d2 = 2000.01;
+        System.out.println("d1: " +d1);
+
+        BigDecimal big1 = BigDecimal.valueOf(d1).stripTrailingZeros();
+        BigDecimal big2 = BigDecimal.valueOf(d2).stripTrailingZeros();
+
+        String test1 = null;
+
+        System.out.println(big1);
+        System.out.println(big1.add(big2));
+
+        System.out.println(big1.toString());
+
+        System.out.println(Double.parseDouble(String.valueOf(big1)));
+
+
+        double d3 = 1000.01;
+        double d4 = 1000.00;
+        double d5 = 0000.01;
+        BigDecimal big3 = BigDecimal.valueOf(d3).stripTrailingZeros();
+        BigDecimal big4 = BigDecimal.valueOf(d4).stripTrailingZeros();
+        BigDecimal big5 = BigDecimal.valueOf(d5).stripTrailingZeros();
+        System.out.println("big3: "+ big3);
+        System.out.println("big4: "+ big4);
+        if(big3.compareTo(big4.add(big5)) == 0){
+            System.out.println("같어");
+        }else{
+            System.out.println("틀려");
+        }
+
+
+        System.out.println();
+        BigDecimal big6 = BigDecimal.ZERO;
+        System.out.println(big6);
+        System.out.println(big6.add(big4));
+
+
+        System.out.println("-----------------------------------------------------");
+        BigDecimal big7 = BigDecimal.valueOf(10000);
+//        BigDecimal n = BigDecimal.valueOf(10);
+//        System.out.println((big7.multiply(BigDecimal.valueOf(9))).divide(BigDecimal.valueOf(100), RoundingMode.FLOOR).setScale(0, RoundingMode.FLOOR));
+        System.out.println((big7.multiply(BigDecimal.valueOf(0.5))).divide(BigDecimal.valueOf(100)).setScale(0, RoundingMode.FLOOR));
+        System.out.println((big7.multiply(BigDecimal.valueOf(0.105))).setScale(0, RoundingMode.FLOOR));
+
+
+        System.out.println();
+        String aaa = "wine_piece_joint_nft";
+        String bbb = "art_piece_joint";
+
+        if(bbb.startsWith("wine")){
+            System.out.println("걸림");
+        }else {
+            System.out.println("안 걸림");
+        }
+        System.out.println();
+
+
+        ArrayList<String> list1 = new ArrayList<String>();
+        for(int i = 0; i < 3; i++){
+            list1.add("테스트"+i);
+        }
+
+        System.out.println(list1);
+
+
+        String sub = "1652428515145-138";
+        System.out.println(sub.split("-")[0]);
+        System.out.println(sub.split("-")[1]);
+
+
+        System.out.println();
+        int resultNftInfo = 1;
+        int resultGrid = 0;
+        if(resultNftInfo < 1 || resultGrid < 1) {
+            System.out.println("걸림");
+        }else {
+            System.out.println("안 걸림");
+        }
+
+        System.out.println();
+
+        BigDecimal total_amt = new BigDecimal(100.123);
+        BigDecimal loyal_amt = BigDecimal.ZERO;
+        BigDecimal loyal_amt2 = new BigDecimal(10);
+
+        System.out.println(total_amt.subtract(loyal_amt2).setScale(2, RoundingMode.FLOOR));
+        System.out.println(total_amt.subtract(loyal_amt2).intValue());
+
+        System.out.println();
+
+        String global_origin_nid_no = "123";
+        String global_origin_grid_no = "123";
+
+        int checkUpdate = 0;
+        if(global_origin_nid_no != null){
+            checkUpdate++;
+            System.out.println("ddd");
+            System.out.println(checkUpdate);
+        }
+        if(global_origin_grid_no != null){
+            checkUpdate = checkUpdate++;
+            System.out.println("ggg");
+            System.out.println(checkUpdate);
+        }
+        System.out.println(checkUpdate);
+
 
 
 
---
+++ src/function/Function1065.java
@@ -0,0 +1,13 @@
+package function;
+
+/*
+*
+* */
+
+// 한수
+public class Function1065 {
+    public static void main(String[] args) {
+
+        
+    }
+}
