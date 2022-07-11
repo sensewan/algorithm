a1f89f31bccb56f5f7a6272f534e0be5478b2496
sensewan <sensewan@gmail.com>
2022-07-11 18:00:43 +0900

    문자열 - 1157 단어 공부 1.아스키코드 2.charAt 3.문자 한 개씩 비교하면서 중복된 값 찾기 및 제일 중복 많이된 글자 찾기

--- src/string/String1157.java
+++ src/string/String1157.java
@@ -1,21 +1,21 @@
+package string;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+
+/*
+* 1.아스키코드
+* 2.charAt
+* 3.문자 한 개씩 비교하면서 중복된 값 찾기 및 제일 중복 많이된 글자 찾기
+* */
+
+// 단어 공부
+public class String1157 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-//        Scanner sc = new Scanner(System.in);
-
-
-//        StringBuilder sb = new StringBuilder();
-//        StringTokenizer st;기
-//        StringTokenizer st2;
-
-
-//        int limit = Integer.parseInt(br.readLine());
 
 //        알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오.
 //        단, 대문자와 소문자를 구분하지 않는다.
@@ -28,8 +28,6 @@ public class Main {
 //        baaa  -> A
 
         String a = br.readLine();
-
-        System.out.println("입력받은 값: " +a);
 
 //        소문자 97 ~ 122
 //        대문자 65 ~ 90
@@ -60,41 +58,7 @@ public class Main {
         }
         System.out.println(ch);
 
-
-
-
-
-
-
-//        System.out.println(s.length());
-
-//        String str = br.readLine();
-
-//        String input = br.readLine();
-//        String input2 = br.readLine();
-//        String line = br.readLine();
-
-//        int a = Integer.parseInt(input.substring(0, input.indexOf(" ")));
-//        int b = Integer.parseInt(input.substring(input.indexOf(" ") + 1));
-
-//        List<List<String>> sList = new ArrayList<List<String>>(limit);
-
-
-
-
-
-
-
-
-
-
         br.close();
 
-//        sc.close();
-
-
     }
-
-
-
 }
