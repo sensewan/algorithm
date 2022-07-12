7fc5eaf881d0dc3082e9909af695b3981079076a
sensewan <sensewan@gmail.com>
2022-07-12 10:51:17 +0900

    문자열 - 1152 단어의 개수

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,57 +14,31 @@ public class Main {
 
 
 //        StringBuilder sb = new StringBuilder();
-//        StringTokenizer st;기
+        StringTokenizer st;
 //        StringTokenizer st2;
 
 
 //        int limit = Integer.parseInt(br.readLine());
-
-//        알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오.
-//        단, 대문자와 소문자를 구분하지 않는다.
-
-//        첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
-
-//        Mississipi    ->  ?
-//        zZa   -> Z
-//        z     -> Z
-//        baaa  -> A
-
         String a = br.readLine();
+        st = new StringTokenizer(a);
 
-        System.out.println("입력받은 값: " +a);
+//        영어 대소문자와 공백으로 이루어진 문자열이 주어진다. 이 문자열에는 몇 개의 단어가 있을까? 이를 구하는 프로그램을 작성하시오.
+//        단, 한 단어가 여러 번 등장하면 등장한 횟수만큼 모두 세어야 한다.
 
-//        소문자 97 ~ 122
-//        대문자 65 ~ 90
-
-        int[] num = new int[26];
+//        첫째 줄에 단어의 개수를 출력한다.
 
 
-        for(int i=0; i<a.length(); i++) {
-            // 소문자
-            if('a' <= a.charAt(i) && a.charAt(i) <='z') {
-                num[a.charAt(i)-97]++;
-            } else {
-                num[a.charAt(i)-65]++;
-            }
+//        The Curious Case of Benjamin Button   ->  6
+//        The first character is a blank    ->  6
+//        The last character is a blank     ->  6
+
+        List<String> list = new ArrayList<>();
+        while (st.hasMoreTokens()) {
+//            System.out.println(st.nextToken(" "));
+            list.add(st.nextToken(" "));
         }
 
-        int max = -1;
-        char ch = '?';
-
-        for (int i = 0; i < num.length; i++) {
-            if (num[i] > max) {
-                max = num[i];
-                ch = (char) (i + 65); // 대문자로 출력해야하므로 65를 더해준다.
-            }
-            else if (num[i] == max) {
-                ch = '?';
-            }
-        }
-        System.out.println(ch);
-
-
-
+        System.out.println(list.size());
 
 
 
@@ -78,14 +55,6 @@ public class Main {
 //        int b = Integer.parseInt(input.substring(input.indexOf(" ") + 1));
 
 //        List<List<String>> sList = new ArrayList<List<String>>(limit);
-
-
-
-
-
-
-
-
 
 
         br.close();
